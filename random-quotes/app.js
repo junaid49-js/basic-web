55c24afe71cf96f43e610c03666a028f32b8115e
Junaid <156448521+junaid49-js@users.noreply.github.com>
2025-05-21 01:06:14 +0530

    Update app.js
    Refine js, will add authors and quotes list arrays

--- random-quotes/app.js
+++ random-quotes/app.js
@@ -2,4 +2,10 @@ const quote = document.getElementById('p');
 const author = document.getElementById('h4');
 const getQuote = document.getElementById('button');
 
-let randomNumber = Math.floor(Math.random() * 100);
+getQuote.addEventListener('click', () => {
+    let randomNumber = Math.floor(Math.random() * 100);
+
+    quote.textContent = `${quotes[randomNumber]}`;
+    author.textContent = `${authors[randomNumber]}`;
+
+})
