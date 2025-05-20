8f35783f4ae07d9e8a18cf45ee141215cb2368b1
Junaid <156448521+junaid49-js@users.noreply.github.com>
2025-05-21 01:05:05 +0530

    Create app.js

---
+++ random-quotes/app.js
@@ -0,0 +1,5 @@
+const quote = document.getElementById('p');
+const author = document.getElementById('h4');
+const getQuote = document.getElementById('button');
+
+let randomNumber = Math.floor(Math.random() * 100);
